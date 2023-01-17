575774276836ba53ca4750cf954cbbb832fc7241
diegoram23 <diego.pta.oc@gmail.com>
2023-01-17 15:43:43 -0800

    further styling to search results

--- index.js
+++ index.js
@@ -19,10 +19,14 @@ let html = ''
 for (let result of resultsArray) {
     console.log(result)
     html += `
-    <div class='result'>
+    <div class='results-container'>
     <img class='img' src='${result.Poster}' />
-    <p>${result.Title}</p>
-    <div/>
+   <div class='results-text'>
+    <p class='result-title'>${result.Title}</p>
+    <p class='result-type'>Type: ${result.Type}</p>
+    <p class='result-year'>Release date: ${result.Year}<span class='add-movie'>Add to watchlist</span></p>
+    </div>
+    </div>
     `
 }
 document.getElementById('start-display').innerHTML = html
