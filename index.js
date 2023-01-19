c4ff701369d4e3bae5ae45baf0a4eb36ccf02ee2
diegoram23 <diego.pta.oc@gmail.com>
2023-01-18 19:30:32 -0800

    styled button when add to watchlist is clicked

--- index.js
+++ index.js
@@ -3,45 +3,44 @@ const searchBtn = document.getElementById('search-btn')
 let resultsArray = []
 let watchlistArray = []
 
-document.addEventListener('click', function(e){
-    if(e.target.dataset.movieid){
-    console.log(e.target.dataset.movieid)
+document.addEventListener('click', function (e) {
+    if (e.target.dataset.movieId) {
+        console.log(e.target.dataset.movieId)
+        addMovieToWatchlist()
     }
 })
 
+searchBtn.addEventListener('click', handleClick)
 
 function handleClick() {
     fetch(`https://www.omdbapi.com/?apikey=9f67eb4&s=${userSearch.value}&i=`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        resultsArray = data.Search
-        renderResults()
-    })
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            resultsArray = data.Search
+            renderResults()
+        })
 }
 
-searchBtn.addEventListener('click',handleClick)
-
-
-
-function renderResults(){
-let html = ''
-for (let result of resultsArray) {
-    console.log(result)
-    html += `
+function renderResults() {
+    let html = ''
+    for (let result of resultsArray) {
+        // console.log(result)
+        html += `
     <div class='results-container'>
     <img class='img' src='${result.Poster}' />
    <div class='results-text'>
     <p class='result-title'>${result.Title}</p>
     <p class='result-type'>Type: ${result.Type}</p>
-    <p class='result-year'>Release: ${result.Year}<span class='add-movie' id='add-movie' data-movieid='${result.imdbID}'>${result.imdbID}</span></p>
+    <p class='result-year'>Release: ${result.Year}<span class='add-movie' id='add-movie' data-movie-id='${result.imdbID}'>Add to watchlist</span></p>
     </div>
     </div>
     `
+    }
+    document.getElementById('start-display').innerHTML = html
 }
 
-document.getElementById('start-display').innerHTML = html
-
+function addMovieToWatchlist(){
+    console.log('yes')
+    document.getElementById('add-movie').innerHTML = 'Added'
 }
-
-
