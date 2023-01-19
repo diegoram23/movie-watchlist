f2b359ae80cf8b25e72ce29cbe9f89d8f4da3f5c
diegoram23 <diego.pta.oc@gmail.com>
2023-01-18 18:56:03 -0800

    event on document listening for clicks on individual search result working

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const userSearch = document.getElementById('user-search')
 const searchBtn = document.getElementById('search-btn')
 let resultsArray = []
+let watchlistArray = []
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.movieid){
+    console.log(e.target.dataset.movieid)
+    }
+})
+
 
 function handleClick() {
-    fetch(`https://www.omdbapi.com/?apikey=9f67eb4&s=${userSearch.value}`)
+    fetch(`https://www.omdbapi.com/?apikey=9f67eb4&s=${userSearch.value}&i=`)
     .then(res => res.json())
     .then(data => {
-        // console.log(data)
+        console.log(data)
         resultsArray = data.Search
         renderResults()
     })
 }
 
 searchBtn.addEventListener('click',handleClick)
+
+
 
 function renderResults(){
 let html = ''
@@ -24,10 +34,14 @@ for (let result of resultsArray) {
    <div class='results-text'>
     <p class='result-title'>${result.Title}</p>
     <p class='result-type'>Type: ${result.Type}</p>
-    <p class='result-year'>Release date: ${result.Year}<span class='add-movie'>Add to watchlist</span></p>
+    <p class='result-year'>Release: ${result.Year}<span class='add-movie' id='add-movie' data-movieid='${result.imdbID}'>${result.imdbID}</span></p>
     </div>
     </div>
     `
 }
+
 document.getElementById('start-display').innerHTML = html
+
 }
+
+
